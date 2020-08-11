85ec2c8181f9fe672cd18f4744021bd5f49288e8
alex-nestor <nestorisena2001@gmail.com>
2020-08-11 15:06:49 +0300

    Added main part

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    $('.photo-slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-l.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-r.svg" alt=""></button>',
+    });
+
+    $('.menu-btn').on('click', function () {
+        $('.menu-list').slideToggle();
+    });
+});
